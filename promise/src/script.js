63e3038e54f223e8f3a57212cf70ed962436adfd
Tim Struthoff <tim.struthoff@gmail.com>
2018-08-01 17:54:37 +0200

    added animation for all items

--- promise/src/script.js
+++ promise/src/script.js
@@ -5,7 +5,7 @@
  */
 function loadImage(url) {
   return new Promise(function(resolve, reject) {
-    var img = document.createElement("img");
+    var img = document.createElement('img');
     img.onload = function() {
       resolve(this);
     };
@@ -28,44 +28,48 @@ function loadImage(url) {
  */
 function animate(element, duration, x, y) {
   return new Promise(function(resolve) {
-    TweenLite.to(element, duration, { x: x, y: y, onComplete: resolve });
+    TweenLite.to(element, duration, {
+      left: x,
+      top: y,
+      onComplete: resolve,
+    });
   });
 }
 
 var images = [
-  "./assets/001-yawn.png",
-  "./assets/002-wink.png",
-  "./assets/003-smile-1.png",
-  "./assets/004-smile.png",
-  "./assets/005-surprise.png",
-  "./assets/006-shocked.png",
-  "./assets/007-sceptic.png",
-  "./assets/008-sad-2.png",
-  "./assets/009-sad-1.png",
-  "./assets/010-happy-3.png",
-  "./assets/011-pain.png",
-  "./assets/012-muted.png",
-  "./assets/013-meh.png",
-  "./assets/014-laugh.png",
-  "./assets/015-ill.png",
-  "./assets/016-happy-2.png",
-  "./assets/017-happy-1.png",
-  "./assets/018-cute.png",
-  "./assets/019-crying.png",
-  "./assets/020-crazy.png",
-  "./assets/021-cool.png",
-  "./assets/022-bored.png",
-  "./assets/023-blush.png",
-  "./assets/024-sad.png",
-  "./assets/025-happy.png"
+  './assets/001-yawn.png',
+  './assets/002-wink.png',
+  './assets/003-smile-1.png',
+  './assets/004-smile.png',
+  './assets/005-surprise.png',
+  './assets/006-shocked.png',
+  './assets/007-sceptic.png',
+  './assets/008-sad-2.png',
+  './assets/009-sad-1.png',
+  './assets/010-happy-3.png',
+  './assets/011-pain.png',
+  './assets/012-muted.png',
+  './assets/013-meh.png',
+  './assets/014-laugh.png',
+  './assets/015-ill.png',
+  './assets/016-happy-2.png',
+  './assets/017-happy-1.png',
+  './assets/018-cute.png',
+  './assets/019-crying.png',
+  './assets/020-crazy.png',
+  './assets/021-cool.png',
+  './assets/022-bored.png',
+  './assets/023-blush.png',
+  './assets/024-sad.png',
+  './assets/025-happy.png',
 ];
 
+const ANIMATION_DURATION = 0.25;
 // Contains all promises of the images loading.
 var promiseArray = [];
 
 images.forEach(currentImageUrl => {
-
-  // Loading image with the current url 
+  // Loading image with the current url
   // and adding th promise for when the loading
   // is finished to the to promise array.
   promiseArray.push(loadImage(currentImageUrl));
@@ -73,4 +77,23 @@ images.forEach(currentImageUrl => {
 
 var allImagesFinishedPromise = Promise.all(promiseArray);
 
-allImagesFinishedPromise.then(image => console.log(image))
+// When all images are finished loading
+allImagesFinishedPromise.then(images => {
+
+  // Appending all images to dom
+  images.forEach(currentImage => {
+    document.body.appendChild(currentImage);
+
+    // Animating all items
+    animate(currentImage, ANIMATION_DURATION, '200px', '0px')
+      .then(() => {
+        return animate(currentImage, ANIMATION_DURATION, '200px', '200px');
+      })
+      .then(() => {
+        return animate(currentImage, ANIMATION_DURATION, '0px', '200px');
+      })
+      .then(() => {
+        return animate(currentImage, ANIMATION_DURATION, '0px', '0px');
+      });
+  });
+});
